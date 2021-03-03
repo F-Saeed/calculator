ce5742d9b873c77070bc10e928e88bd0c04f47de
Faran Saeed <faransaeed@rocketmail.com>
2021-03-03 16:46:10 +0500

    Completed the calculator app

--- app.js
+++ app.js
@@ -5,85 +5,145 @@ let multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
 let divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
 
 let operate = (firstNumber, secondNumber, operation) => {
-  return operation === 'add' || operation === '+'
-    ? add(firstNumber, secondNumber)
-    : operation === 'subtract' || operation === '-'
-    ? subtract(firstNumber, secondNumber)
-    : operation === 'multiply' || operation === '*'
-    ? multiply(firstNumber, secondNumber)
-    : divide(firstNumber, secondNumber);
+  switch (operation) {
+    case '+':
+      return add(firstNumber, secondNumber);
+      break;
+
+    case '-':
+      return subtract(firstNumber, secondNumber);
+      break;
+
+    case '*':
+      return multiply(firstNumber, secondNumber);
+      break;
+
+    case '/':
+      return divide(firstNumber, secondNumber);
+      break;
+  }
 };
 
-let percentage = (number) => (number * 0.01).toString();
-let backSpace = (str) => str.slice(0, -1);
+/* Operations for different classes */
+let itemNumber = (operator, attr) => {
+  if (!operator) {
+    firstNumber += parseFloat(attr);
+    firstNumber = firstNumber.substring(0, 13);
+    $('#input').text(firstNumber);
+  } else {
+    secondNumber += parseFloat(attr);
+    secondNumber = secondNumber.substring(0, 13);
+    $('#input').text(secondNumber);
+  }
+};
 
+let itemOperator = (firstNumber, secondNumber, attr) => {
+  if (firstNumber && !secondNumber) {
+    operator = attr;
+    history = `${firstNumber} ${operator}`.substring(0, 15);
+
+    $('#history').text(history);
+    $('#input').empty();
+  }
+};
+
+let itemDecimal = () => {
+  if (firstNumber && !secondNumber) {
+    firstNumber = `${firstNumber.toString()}` + '.';
+    $('#input').text(firstNumber);
+  } else {
+    secondNumber = `${secondNumber.toString()}` + '.';
+    $('#input').text(secondNumber);
+  }
+};
+
+/* Operations for different ids */
+
+let equal = (firstNumber, secondNumber, operator) => {
+  if (firstNumber && secondNumber) {
+    firstNumber = operate(firstNumber, secondNumber, operator).toExponential(7);
+
+    $('#input').text(firstNumber);
+    $('#history').empty();
+
+    secondNumber = '';
+    operator = '';
+  }
+};
+
+let clear = () => {
+  $('#history').empty();
+  $('#input').empty();
+
+  operator = '';
+  firstNumber = '';
+  secondNumber = '';
+};
+
+let backSpace = () => {
+  if (firstNumber && !secondNumber) {
+    firstNumber = firstNumber.slice(0, -1);
+    $('#input').text(firstNumber);
+  } else {
+    secondNumber = secondNumber.slice(0, -1);
+    $('#input').text(secondNumber);
+  }
+};
+
+let percentage = () => {
+  if (firstNumber && !secondNumber) {
+    firstNumber = firstNumber * 0.01;
+    $('#input').text(firstNumber);
+  } else {
+    secondNumber = secondNumber * 0.01;
+    $('#input').text(secondNumber);
+  }
+};
+
+/* Declaring empty variables */
 let history = '';
 let firstNumber = '';
 let secondNumber = '';
+let answer = 0;
 let operator = '';
 
+/* Calculations */
 $(document).click((event) => {
   if (event.target.className === 'item number') {
-    if (!operator) {
-      firstNumber += event.target.id;
-      $('#input').text(firstNumber);
-    } else {
-      secondNumber += event.target.id;
-      $('#input').text(secondNumber);
-    }
+    itemNumber(operator, event.target.id);
   } else if (event.target.className === 'item operator') {
-    if (firstNumber && !secondNumber) {
-      operator = event.target.id;
-      history = `${firstNumber} ${operator}`;
-
-      $('#history').text(history);
-      $('#input').empty();
-    }
+    itemOperator(firstNumber, secondNumber, event.target.id);
   } else if (event.target.className === 'item other') {
-    firstNumber = parseFloat(firstNumber);
-    secondNumber = parseFloat(secondNumber);
-
     if (event.target.id === 'equal') {
-      if (firstNumber && secondNumber) {
-        firstNumber = operate(firstNumber, secondNumber, operator);
-
-        $('#input').text(firstNumber);
-        $('#history').empty();
-
-        secondNumber = '';
-        operator = '';
-      }
+      equal(firstNumber, secondNumber, operator);
     } else if (event.target.id === 'clear') {
-      $('#history').empty();
-      $('#input').empty();
-
-      operator = '';
-      firstNumber = '';
-      secondNumber = '';
+      clear();
     } else if (event.target.id === 'backspace') {
-      if (firstNumber && !secondNumber) {
-        firstNumber = backSpace(firstNumber.toString());
-        $('#input').text(firstNumber);
-      } else {
-        secondNumber = backSpace(secondNumber.toString());
-        $('#input').text(secondNumber);
-      }
+      backSpace();
     } else if (event.target.id === '%') {
-      if (firstNumber && !secondNumber) {
-        firstNumber = percentage(firstNumber);
-        $('#input').text(firstNumber);
-      } else {
-        secondNumber = percentage(secondNumber);
-        $('#input').text(secondNumber);
-      }
+      percentage();
     }
-  } else if (event.target.id === '.') {
-    if (firstNumber && !secondNumber) {
-      firstNumber = `${firstNumber.toString()}` + '.';
-      $('#input').text(firstNumber);
-    } else {
-      secondNumber = `${secondNumber.toString()}` + '.';
-      $('#input').text(secondNumber);
+  } else if (event.target.className === 'item decimal') {
+    itemDecimal(firstNumber, secondNumber);
+  }
+});
+
+$(document).keypress((event) => {
+  if ($(`#${event.key}`).hasClass('item number')) {
+    itemNumber(operator, event.key);
+  } else if ($(`#${event.key}`).hasClass('item operator')) {
+    itemOperator(firstNumber, secondNumber, event.key);
+  } else if ($(`#${event.key}`).hasClass('item other')) {
+    if (event.target.id === 'equal') {
+      equal(firstNumber, secondNumber, operator);
+    } else if (event.target.id === 'clear') {
+      clear();
+    } else if (event.target.id === 'backspace') {
+      backSpace();
+    } else if (event.target.id === '%') {
+      percentage();
     }
+  } else if ($(`#${event.key}`).hasClass('item decimal')) {
+    itemDecimal(firstNumber, secondNumber);
   }
 });
